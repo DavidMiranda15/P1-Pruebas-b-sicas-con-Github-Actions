617fdcfe3303dcfa88d3f8ff6e1e32c4891e4d95
Devidmiranda15 <luis.miranda68@uabc.edu.mx>
2026-04-23 17:22:29 -0700

    Añadiendo nuevo metodo de pruebaaa

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,3 +2,6 @@ from calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+def test_subtract():
+    calc = Calculadora()
+    assert calc.subtract(10, 5) == 5
